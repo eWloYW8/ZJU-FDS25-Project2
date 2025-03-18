40894b5caf7535ade69b452a1ed35db2b0c16fa3
eWloYW8 <3171132517@qq.com>
2025-03-18 04:18:58 +0000

    None

--- code/simplificator/monomial.c
+++ code/simplificator/monomial.c
@@ -5,35 +5,3 @@
 #include "../ads/hashtable.h"
 
 #include <stdlib.h>
-
-Monomial* create_monomial() {
-    Monomial* monomial = (Monomial*)malloc(sizeof(Monomial));
-    monomial->coefficient = 1;
-    monomial->coefficient_denominator = 1;
-    monomial->expressionobjects = create_hash_table(MONOMIAL_SIZE);
-    return monomial;
-}
-
-void free_monomial(Monomial* monomial) {
-    free_hash_table(monomial->expressionobjects);
-    free(monomial);
-}
-
-Monomial* deep_copy_monomial(Monomial* monomial) {
-    Monomial* new_monomial = create_monomial();
-    new_monomial->coefficient = monomial->coefficient;
-    new_monomial->coefficient_denominator = monomial->coefficient_denominator;
-    for (unsigned long long i = 0; i < monomial->expressionobjects->size; ++i) {
-        HashTableEntry* entry = monomial->expressionobjects->buckets[i];
-        while (entry) {
-            insert(new_monomial->expressionobjects, entry->obj);
-            entry = entry->next;
-        }
-    }
-    return new_monomial;
-}
-
-
-
-
-
